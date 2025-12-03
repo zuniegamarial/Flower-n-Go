717ae5e0b4fda0babb9aeb8333af5df4ec54931e
marialzuniega <marialangelmadorozuniega@gmail.com>
2025-12-03 23:04:42 +0800

    Added Place order, Success, Modified other files

---
+++ success.php
@@ -0,0 +1,11 @@
+<style>
+body{text-align:center;background:#111;color:white}
+.box{margin:100px auto;max-width:500px;background:#222;padding:30px}
+a{color:gold}
+</style>
+
+<div class="box">
+<h1>✅ Order Placed!</h1>
+<p>Your flowers are being prepared.</p>
+<a href="dashboard.php">Back to Shop</a>
+</div>
